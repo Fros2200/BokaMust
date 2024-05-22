e9ef5dfdfc89a06bb541799c354e46d56a74e1f7
Frida Östberg <117045810+Fros2200@users.noreply.github.com>
2024-05-22 11:04:27 +0200

    #36 påbörjat bekräftelse av tidsbokning

--- BokaMust/Controllers/BookingController.cs
+++ BokaMust/Controllers/BookingController.cs
@@ -8,96 +8,119 @@ namespace BokaMust.Controllers
     public class BookingController : Controller
     {
 
-        private List<TimeSlot> GetTimeSlots() 
-        {
-            return new List<TimeSlot>
-            {
-                new TimeSlot ("Augusti","15","09:00 - 09:30",0.5),
-                new TimeSlot ("Augusti","15","09:30 - 10:00",0.5),
-                new TimeSlot ("Augusti","15","10:00 - 11:00",1),
-                new TimeSlot ("Augusti","15","11:00 - 13:00",2),
-                new TimeSlot ("Augusti","15","13:00 - 16:00",3),
-                new TimeSlot ("Augusti","15","16:00 - 20:00",4),
-                new TimeSlot ("Augusti","16","09:00 - 14:00",5),
-                new TimeSlot ("Augusti","17","09:00 - 09:30",0.5),
-                new TimeSlot ("Augusti","17","09:30 - 10:00",0.5),
+		#region Action methods
 
-                new TimeSlot ("September","15","09:00 - 09:30",0.5),
-                new TimeSlot ("September","15","09:30 - 10:00",0.5),
-                new TimeSlot ("September","15","10:00 - 11:00",1),
-                new TimeSlot ("September","15","11:00 - 13:00",2),
-                new TimeSlot ("September","15","13:00 - 16:00",3),
-                new TimeSlot ("September","15","16:00 - 20:00",4),
-                new TimeSlot ("September","16","09:00 - 14:00",5),
-                new TimeSlot ("September","17","09:00 - 09:30",0.5),
-                new TimeSlot ("September","17","09:30 - 10:00",0.5),
+		public IActionResult ShowCalendar()
+		{
 
-                new TimeSlot ("Oktober","15","09:00 - 09:30",0.5),
-                new TimeSlot ("Oktober","15","09:30 - 10:00",0.5),
-                new TimeSlot ("Oktober","15","10:00 - 11:00",1),
-                new TimeSlot ("Oktober","15","11:00 - 13:00",2),
-                new TimeSlot ("Oktober","15","13:00 - 16:00",3),
-                new TimeSlot ("Oktober","15","16:00 - 20:00",4),
-                new TimeSlot ("Oktober","16","09:00 - 14:00",5),
-                new TimeSlot ("Oktober","17","09:00 - 09:30",0.5),
-                new TimeSlot ("Oktober","17","09:30 - 10:00",0.5),
+			return View("Calendar");
+		}
 
-                new TimeSlot ("November","15","09:00 - 09:30",0.5),
-                new TimeSlot ("November","15","09:30 - 10:00",0.5),
-                new TimeSlot ("November","15","10:00 - 11:00",1),
-                new TimeSlot ("November","15","11:00 - 13:00",2),
-                new TimeSlot ("November","15","13:00 - 16:00",3),
-                new TimeSlot ("November","15","16:00 - 20:00",4),
-                new TimeSlot ("November","16","09:00 - 14:00",5),
-                new TimeSlot ("November","17","09:00 - 09:30",0.5),
-                new TimeSlot ("November","17","09:30 - 10:00",0.5),
-            };
-        }
+		public IActionResult BookingForm()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult ConfirmTimeSlot(TimeSlot timeSlot)
+		{
+
+			return View("BookingConfirm", timeSlot);
+		}
+
+		[HttpPost]
+		public IActionResult BookTimeSlot(TimeSlot timeSlot)
+		{
+
+			return View("BookingConfirmation", timeSlot);
+		}
+
+		public IActionResult AvailableTimes()
+		{
+			var modelJson = HttpContext.Session.GetString("GuideResults");
+			if (string.IsNullOrEmpty(modelJson))
+			{
+				return RedirectToAction("Error");
+			}
+
+			var guideViewModel = JsonSerializer.Deserialize<GuideViewModel>(modelJson);
+			var availableTimes = FindAvailableTimeSlots(guideViewModel);
+
+			return View("Calendar", availableTimes);
+		}
+
+		#endregion
+
+		#region Helper methods
+
+		private List<TimeSlot> GetTimeSlots()
+		{
+			return new List<TimeSlot>
+			{
+				new TimeSlot ("Augusti","15","09:00 - 09:30",0.5),
+				new TimeSlot ("Augusti","15","09:30 - 10:00",0.5),
+				new TimeSlot ("Augusti","15","10:00 - 11:00",1),
+				new TimeSlot ("Augusti","15","11:00 - 13:00",2),
+				new TimeSlot ("Augusti","15","13:00 - 16:00",3),
+				new TimeSlot ("Augusti","15","16:00 - 20:00",4),
+				new TimeSlot ("Augusti","16","09:00 - 14:00",5),
+				new TimeSlot ("Augusti","17","09:00 - 09:30",0.5),
+				new TimeSlot ("Augusti","17","09:30 - 10:00",0.5),
+
+				new TimeSlot ("September","15","09:00 - 09:30",0.5),
+				new TimeSlot ("September","15","09:30 - 10:00",0.5),
+				new TimeSlot ("September","15","10:00 - 11:00",1),
+				new TimeSlot ("September","15","11:00 - 13:00",2),
+				new TimeSlot ("September","15","13:00 - 16:00",3),
+				new TimeSlot ("September","15","16:00 - 20:00",4),
+				new TimeSlot ("September","16","09:00 - 14:00",5),
+				new TimeSlot ("September","17","09:00 - 09:30",0.5),
+				new TimeSlot ("September","17","09:30 - 10:00",0.5),
+
+				new TimeSlot ("Oktober","15","09:00 - 09:30",0.5),
+				new TimeSlot ("Oktober","15","09:30 - 10:00",0.5),
+				new TimeSlot ("Oktober","15","10:00 - 11:00",1),
+				new TimeSlot ("Oktober","15","11:00 - 13:00",2),
+				new TimeSlot ("Oktober","15","13:00 - 16:00",3),
+				new TimeSlot ("Oktober","15","16:00 - 20:00",4),
+				new TimeSlot ("Oktober","16","09:00 - 14:00",5),
+				new TimeSlot ("Oktober","17","09:00 - 09:30",0.5),
+				new TimeSlot ("Oktober","17","09:30 - 10:00",0.5),
+
+				new TimeSlot ("November","15","09:00 - 09:30",0.5),
+				new TimeSlot ("November","15","09:30 - 10:00",0.5),
+				new TimeSlot ("November","15","10:00 - 11:00",1),
+				new TimeSlot ("November","15","11:00 - 13:00",2),
+				new TimeSlot ("November","15","13:00 - 16:00",3),
+				new TimeSlot ("November","15","16:00 - 20:00",4),
+				new TimeSlot ("November","16","09:00 - 14:00",5),
+				new TimeSlot ("November","17","09:00 - 09:30",0.5),
+				new TimeSlot ("November","17","09:30 - 10:00",0.5),
+			};
+		}
+
+		public List<TimeSlot> FindAvailableTimeSlots(GuideViewModel guideViewModel)
+		{
+			var selectedApple = guideViewModel.Apples?.FirstOrDefault(a => a.Name == guideViewModel.SelectedApple);
+
+			string harvestMonth = selectedApple.HarvestMonth;
+
+			var allSlots = GetTimeSlots();
+			var matchedSlots = allSlots.Where(slot =>
+			slot.Month == harvestMonth && slot.Duration == guideViewModel.SessionTime).ToList();
+
+			return matchedSlots;
+		}
+
+		#endregion
 
 
-        public IActionResult BookingForm()
-        {
-            return View();
-        }
+		//[HttpPost]
+		//public IActionResult BookTimeSlot(TimeSlot timeSlot)
+		//{
 
-        public IActionResult AvailableTimes()
-        {
-            var modelJson = HttpContext.Session.GetString("GuideResults");
-            if (string.IsNullOrEmpty(modelJson))
-            {
-                return RedirectToAction("Error");
-            }
+		//    return View("BookingConfirmation", timeSlot);
+		//}
 
-            var guideViewModel = JsonSerializer.Deserialize<GuideViewModel>(modelJson);
-            var availableTimes = FindAvailableTimeSlots(guideViewModel);
-
-            return View("Calendar", availableTimes);
-        }
-
-        public List<TimeSlot> FindAvailableTimeSlots(GuideViewModel guideViewModel)
-        {
-            var selectedApple = guideViewModel.Apples?.FirstOrDefault(a => a.Name == guideViewModel.SelectedApple);
-
-            string harvestMonth = selectedApple.HarvestMonth; 
-
-            var allSlots = GetTimeSlots();
-            var matchedSlots = allSlots.Where(slot =>
-            slot.Month == harvestMonth && slot.Duration == guideViewModel.SessionTime).ToList(); 
-
-            return matchedSlots; 
-        }
-
-        [HttpPost]
-        public IActionResult BookTimeSlot(TimeSlot timeSlot)
-        {
-            
-            return View("BookingConfirmation", timeSlot);
-        }
-
-        public IActionResult ShowCalendar()
-        { 
-            
-            return View("Calendar"); 
-        }
-    }
+	}
 }
